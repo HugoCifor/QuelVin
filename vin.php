d3d39e1aabe2093e38e36fa7180e0939ef207565
hugo <hugo.cifor@gmail.com>
2023-02-23 12:39:04 +0100

    premier commit

---
+++ vin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/vin.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="navbar"></div>
+
+    <div class="hidden"> 
+        <div>about us</div>
+        <div>bla</div>
+        <div>bla bla</div>
+    </div>
+
+    <div class="test"></div>
+
+    <div class="main">
+            <div id='choix' class="choix1">
+                <div id='choixTitre' class="choix1Titre"></div>
+                <div id='choixMain' class="choix1Main"></div>
+                <div id='choixText' class="choix1Text"></div>
+            </div>
+            <div id='choix' class="choix2">
+                <div id='choixTitre' class="choix2Titre"></div>
+                <div id='choixMain' class="choix2Main"></div>
+                <div id='choixText' class="choix2Text"></div>
+            </div>
+            <div id='choix' class="choix3">
+                <div id='choixTitre' class="choix3Titre"></div>
+                <div id='choixMain' class="choix3Main"></div>
+                <div id='choixText'  class="choix3Text"></div>
+            </div>
+    </div>
+
+    <footer></footer>
+</body>
+</html>
